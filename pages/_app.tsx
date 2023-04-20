32a86d88403341e5783a8f2ddd0d1f9eb00bc119
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2023-04-20 15:06:57 +0800

    tutorial four

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, MouseEvent } from "react";
 
 export default function About() {
+  const [message, setMessage] = useState<string>("");
+
+  const handlerEvent = async () => {
+    const res = await fetch("/api/hello");
+    const data = await res.json();
+    // console.log(res, data);
+    setMessage(JSON.stringify(data, null, 4));
+  };
+
+  const btn_click = async (event: MouseEvent<HTMLButtonElement>) => {
+    await handlerEvent();
+  };
+
   return (
-    <div>About</div>
-  )
+    <div>
+      <h1>About</h1>
+      <hr />
+      <div>
+        <button className="btn btn-success" onClick={(e) => btn_click(e)}>
+          數據取得
+        </button>
+        <pre>{message}</pre>
+      </div>
+    </div>
+  );
 }
